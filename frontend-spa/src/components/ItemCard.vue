61fdcc991e61b76f37759089edcd228e3ffd0858
ryo-endo <to.endo.dev@gmail.com>
2018-08-03 08:09:04 +0900

    Homeページにアイテムリストを表示する。

---
+++ frontend-spa/src/components/ItemCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="itemcard">
+    <h1>{{item.Name}}</h1>
+    <h2>{{item.Description}}</h2>
+    <h2>{{item.Amount}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ItemCard',
+  props: [
+    'item'
+  ]
+}
+</script>
+
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
