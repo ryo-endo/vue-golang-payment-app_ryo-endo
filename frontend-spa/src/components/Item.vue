439c6c9910bdf076046b419fe3af23533602aa9d
ryo-endo <to.endo.dev@gmail.com>
2018-08-03 08:35:00 +0900

    Item詳細ページを作成。

---
+++ frontend-spa/src/components/Item.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="helllo">
+    ddd
+    <h1>{{item.Name}}</h1>
+    <h2>{{item.Description}}</h2>
+    <h2>{{item.Amount}}円</h2>
+    <router-link to="/">HOMEへ</router-link>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'ItemCard',
+  data () {
+    return {
+      item: {}
+    }
+  },
+  created () {
+    axios.get(`http://localhost:8888/api/v1/items/${this.$route.params.id}`).then(res => {
+      console.log(res.data)
+      this.item = res.data
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
